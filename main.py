c84920efafda4ce2e44a33717bf0e8b22725c452
GavinDavidson24 <818399@mystma.org>
2021-09-28 15:09:52 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    if abs(10 - x) > abs(10 - y):
+        return y
+    elif abs(10 - x) == abs(10 - y):
+        return 0
+    else:
+        return x
+print(close10(11, 15))
